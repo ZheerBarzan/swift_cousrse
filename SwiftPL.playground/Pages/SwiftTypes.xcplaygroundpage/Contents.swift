0cf92dc329dd92765b83a9419bba90c0f5494e60
zheer barzan <zheerbarzan85@outlook.com>
2024-11-21 18:47:29 +0300

    swift types and setting up codium

--- SwiftPL.playground/Pages/SwiftTypes.xcplaygroundpage/Contents.swift
+++ SwiftPL.playground/Pages/SwiftTypes.xcplaygroundpage/Contents.swift
@@ -1,7 +1,27 @@
-//: [Previous](@previous)
+
 
 import Foundation
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+// String is "reguler text"
+let text: String = "this is a string"
+
+// Boolean AkA bool is either true or false
+let choice: Bool = true
+
+// Date is 10/aug/2024
+let date: Date = Date()
+
+// Int is an Integer like 4,3,2,1
+let number: Int = 80085
+
+//Double is a floating point number like 3.14
+let number2:Double = 6.9
+
+print(text)
+print(choice)
+print(date)
+print(number)
+print(number2)
+
+
