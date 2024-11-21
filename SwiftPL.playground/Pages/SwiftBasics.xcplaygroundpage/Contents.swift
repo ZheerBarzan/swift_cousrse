12b6cd620f1fd139202d1c43c062d552525cde92
zheer barzan <zheerbarzan85@outlook.com>
2024-11-21 17:52:46 +0300

    swift basics done

--- SwiftPL.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
+++ SwiftPL.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
-//: [Previous](@previous)
+
 
 import Foundation
 
-var greeting = "Hello, playground"
+var greeting = "Hello, zheer"
+// this is a commnet
+/*
+ this is the muliti line commnet
+ */
 
-//: [Next](@next)
+
+
+// naming convention
+
+// camel case
+let sayHello = "Hello, "
+var userName = "zheer"
+
+let thisIsWhatCamelCaseLooksLike = "camelCase \n"
+
+// pascal Case
+let ThisIsWhatPascalCaseLooksLike = "PascalCase \n"
+
+// snake case
+let this_is_what_snake_case_looks_like = "snake_case \n"
+
+// don't use case
+let thisiswhatdontusecaselookslike = "dontusecase \n"
+
+// print something to the console
+print(greeting)
+print(sayHello + userName)
+
+print(thisIsWhatCamelCaseLooksLike + ThisIsWhatPascalCaseLooksLike + this_is_what_snake_case_looks_like+thisiswhatdontusecaselookslike)
