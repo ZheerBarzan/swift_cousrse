db9dd49243f8ff616e24a2831465cdeeafda8bee
zheer barzan <zheerbarzan85@outlook.com>
2024-11-26 17:28:17 +0300

    tuples coverd

--- SwiftPL.playground/Pages/SwiftTuples.xcplaygroundpage/Contents.swift
+++ SwiftPL.playground/Pages/SwiftTuples.xcplaygroundpage/Contents.swift
@@ -1,7 +1,20 @@
-//: [Previous](@previous)
+ let name = "zheer"
+ let isCool = true
 
-import Foundation
 
-var greeting = "Hello, playground"
+func getUser() -> ( String, Bool) {
+    
+    let user = ( "aland",  false)
+    return (user.0, user.1)
+}
 
-//: [Next](@next)
+var userName = getUser().0
+var isUserCool = getUser().1
+
+
+func getUser2() -> (name: String,  isCool: Bool) {
+    return (name, isCool)
+}
+
+var userName2 = getUser2().name
+var isUserCool2 = getUser2().isCool
