e5556fdafd24e1fc29ab22e6bc701803d6157d81
zheer barzan <zheerbarzan85@outlook.com>
2024-12-02 17:42:34 +0300

    making a tempureter convertor

---
+++ SwiftPL.playground/Pages/TempretureConverter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+
+let celcius = 30
+let fahrenheit = celcius * 9 / 5 + 32
+let kelvin = celcius + 273
+
+print("tempreture in celcius \(celcius)° = \(fahrenheit)° in fahrenheit")
+print("tempreture in celcius \(celcius)° = \(kelvin)° in kelvin")
